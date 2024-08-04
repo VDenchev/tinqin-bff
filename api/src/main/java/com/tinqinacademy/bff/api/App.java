c3705bf460558abdcfc9fc3391387d6f1ac44119
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-04 23:04:34 +0300

    Initial commit

---
+++ api/src/main/java/com/tinqinacademy/bff/api/App.java
@@ -0,0 +1,13 @@
+package com.tinqinacademy.bff.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
