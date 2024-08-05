a4f1d1e501b702955f7da498fe50ed635fb7d48b
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-05 17:23:27 +0300

    [TB-3] Configure the comments feign client

---
+++ api/src/main/java/com/tinqinacademy/bff/api/configuration/JacksonConfig.java
@@ -0,0 +1,20 @@
+package com.tinqinacademy.bff.api.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    return new ObjectMapper().registerModules(new JavaTimeModule())
+        .enable(SerializationFeature.INDENT_OUTPUT)
+        .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .disable(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS);
+  }
+}
