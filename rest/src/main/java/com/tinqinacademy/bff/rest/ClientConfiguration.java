8577d88433f59d6ef34d0486c184237f68bc1c55
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-07 00:07:59 +0300

    [TB-6] Add, configure and test the hotel feign client from hotel service (#7)
    * [TB-6] Add dependency for the hotel feign client from the hotel service
    
    * [TB-6] Configure the hotel client and test making http requests to it

--- rest/src/main/java/com/tinqinacademy/bff/rest/ClientConfiguration.java
+++ rest/src/main/java/com/tinqinacademy/bff/rest/ClientConfiguration.java
@@ -8,19 +8,29 @@ import feign.jackson.JacksonEncoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.tinqinacademy.hotel.restexport.client.HotelClient;
 
 @Configuration
 @RequiredArgsConstructor
 public class ClientConfiguration {
 
   private static final String COMMENTS_CLIENT_URL = "http://localhost:8081";
+  private static final String HOTEL_CLIENT_URL = "http://localhost:8080";
   private final ObjectMapper objectMapper;
 
   @Bean
-  public CommentsClient getCommentsClient() {
+  public CommentsClient commentsClient() {
     return Feign.builder()
         .encoder(new JacksonEncoder(objectMapper))
         .decoder(new JacksonDecoder(objectMapper))
         .target(CommentsClient.class, COMMENTS_CLIENT_URL);
+  }
+
+  @Bean
+  public HotelClient hotelClient() {
+    return Feign.builder()
+        .encoder(new JacksonEncoder(objectMapper))
+        .decoder(new JacksonDecoder(objectMapper))
+        .target(HotelClient.class, HOTEL_CLIENT_URL);
   }
 }
