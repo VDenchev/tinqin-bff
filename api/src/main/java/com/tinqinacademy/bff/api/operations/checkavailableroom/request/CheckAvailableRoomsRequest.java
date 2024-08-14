b65f90aac2112326a88454e71f00594b384f162a
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-14 23:06:31 +0300

    [TB-8] Add check available rooms operation and request and response POJOS

---
+++ api/src/main/java/com/tinqinacademy/bff/api/operations/checkavailableroom/request/CheckAvailableRoomsRequest.java
@@ -0,0 +1,29 @@
+package com.tinqinacademy.bff.api.operations.checkavailableroom.request;
+
+import com.tinqinacademy.bff.api.base.OperationRequest;
+import com.tinqinacademy.bff.api.enums.BathroomType;
+import com.tinqinacademy.bff.api.enums.BedType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class CheckAvailableRoomsRequest implements OperationRequest {
+
+  private Integer bedCount;
+  private BathroomType bathroomType;
+  private List<BedType> bedSizes;
+  private LocalDate startDate;
+  private LocalDate endDate;
+}
---
+++ api/src/main/java/com/tinqinacademy/bff/api/operations/checkavailableroom/response/CheckAvailableRoomsResponse.java
@@ -0,0 +1,24 @@
+package com.tinqinacademy.bff.api.operations.checkavailableroom.response;
+
+import com.tinqinacademy.bff.api.base.OperationResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class CheckAvailableRoomsResponse implements OperationResponse {
+
+  @Schema(example = "[\"uuid1\", \"uuid2\"]")
+  private List<String> roomIds;
+}
