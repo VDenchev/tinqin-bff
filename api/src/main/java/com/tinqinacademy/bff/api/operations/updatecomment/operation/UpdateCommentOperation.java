194d31dceeb4599d4aa6b0564935e2cfbaca9655
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-14 23:14:13 +0300

    [TB-8] Add update comment operation and request and response POJOS

---
+++ api/src/main/java/com/tinqinacademy/bff/api/operations/updatecomment/operation/UpdateCommentOperation.java
@@ -0,0 +1,8 @@
+package com.tinqinacademy.bff.api.operations.updatecomment.operation;
+
+import com.tinqinacademy.bff.api.base.Operation;
+import com.tinqinacademy.bff.api.operations.updatecomment.request.UpdateCommentRequest;
+import com.tinqinacademy.bff.api.operations.updatecomment.response.UpdateCommentResponse;
+
+public interface UpdateCommentOperation extends Operation<UpdateCommentRequest, UpdateCommentResponse> {
+}
