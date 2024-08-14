799a7dd6c688674ffe6fb944b39e90cfdf4fc03c
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-14 23:12:29 +0300

    [TB-8] Add register visitors operation and request and response POJOS

---
+++ api/src/main/java/com/tinqinacademy/bff/api/operations/registervisitors/response/RegisterVisitorsResponse.java
@@ -0,0 +1,16 @@
+package com.tinqinacademy.bff.api.operations.registervisitors.response;
+
+import com.tinqinacademy.bff.api.base.OperationResponse;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@ToString
+public class RegisterVisitorsResponse implements OperationResponse {
+}
