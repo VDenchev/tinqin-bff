ef80b755febdb82639e0b65d5d78003fdbfc9596
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-14 23:13:55 +0300

    [TB-8] Add update comment by admin operation and request and response POJOS

---
+++ api/src/main/java/com/tinqinacademy/bff/api/operations/updatecomment/updatecommentbyadmin/operation/UpdateCommentByAdminOperation.java
@@ -0,0 +1,8 @@
+package com.tinqinacademy.bff.api.operations.updatecomment.updatecommentbyadmin.operation;
+
+import com.tinqinacademy.bff.api.base.Operation;
+import com.tinqinacademy.bff.api.operations.updatecomment.updatecommentbyadmin.request.UpdateCommentByAdminRequest;
+import com.tinqinacademy.bff.api.operations.updatecomment.updatecommentbyadmin.response.UpdateCommentByAdminResponse;
+
+public interface UpdateCommentByAdminOperation extends Operation<UpdateCommentByAdminRequest, UpdateCommentByAdminResponse> {
+}
