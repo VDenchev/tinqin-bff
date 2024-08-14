71d6d2e3d26eb5b682e97a82e8ed680ce7283c76
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-14 23:07:26 +0300

    [TB-8] Add check deleteroom operation and request and response POJOS

---
+++ api/src/main/java/com/tinqinacademy/bff/api/operations/deletecomment/response/DeleteCommentResponse.java
@@ -0,0 +1,16 @@
+package com.tinqinacademy.bff.api.operations.deletecomment.response;
+
+import com.tinqinacademy.bff.api.base.OperationResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@ToString
+public class DeleteCommentResponse implements OperationResponse {
+}
