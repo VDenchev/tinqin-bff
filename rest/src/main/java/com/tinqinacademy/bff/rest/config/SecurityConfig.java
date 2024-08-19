8e0af5417d99f378fb23fc2f40d9d38d8ceb899e
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-19 13:41:23 +0300

    [TB-14] Fix bugs

--- rest/src/main/java/com/tinqinacademy/bff/rest/config/SecurityConfig.java
+++ rest/src/main/java/com/tinqinacademy/bff/rest/config/SecurityConfig.java
@@ -17,6 +17,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -50,7 +52,7 @@ public class SecurityConfig {
                 "/v3/api-docs/**",
                 "/swagger-ui/**",
                 "/swagger-ui.html").permitAll()
-            .anyRequest().permitAll())
+            .anyRequest().authenticated())
         .httpBasic(Customizer.withDefaults())
         .sessionManagement(s ->
             s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
