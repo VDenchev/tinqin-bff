f75048f6a604fec548d3230312831a7f9e20f4af
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-20 23:40:06 +0300

    [TB-18] Add logic in update room processor

--- core/src/main/java/com/tinqinacademy/bff/core/processors/UpdateRoomOperationProcessor.java
+++ core/src/main/java/com/tinqinacademy/bff/core/processors/UpdateRoomOperationProcessor.java
@@ -4,32 +4,67 @@ import com.tinqinacademy.bff.api.errors.ErrorResponse;
 import com.tinqinacademy.bff.api.operations.updateroom.operation.UpdateRoomOperation;
 import com.tinqinacademy.bff.api.operations.updateroom.request.UpdateRoomRequest;
 import com.tinqinacademy.bff.api.operations.updateroom.response.UpdateRoomResponse;
+import com.tinqinacademy.hotel.api.models.input.RoomInput;
+import com.tinqinacademy.hotel.api.operations.updateroom.input.UpdateRoomInput;
+import com.tinqinacademy.hotel.api.operations.updateroom.output.UpdateRoomOutput;
+import com.tinqinacademy.hotel.restexport.client.HotelClient;
 import io.vavr.control.Either;
 import io.vavr.control.Try;
 import jakarta.validation.Validator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
 import static io.vavr.API.Match;
 
+@Slf4j
 @Service
 public class UpdateRoomOperationProcessor extends BaseOperationProcessor implements UpdateRoomOperation {
 
-  public UpdateRoomOperationProcessor(ConversionService conversionService, Validator validator) {
+  private final HotelClient hotelClient;
+
+  public UpdateRoomOperationProcessor(
+      ConversionService conversionService, Validator validator,
+      HotelClient hotelClient
+  ) {
     super(conversionService, validator);
+    this.hotelClient = hotelClient;
   }
 
   @Override
-  public Either<? extends ErrorResponse, UpdateRoomResponse> process(UpdateRoomRequest input) {
-    return validateInput(input)
-        .flatMap(validInput ->
+  public Either<? extends ErrorResponse, UpdateRoomResponse> process(UpdateRoomRequest request) {
+    return validateInput(request)
+        .flatMap(validRequest ->
             Try.of(() -> {
-                  return UpdateRoomResponse.builder().build();
+                  log.info("Start update room input: {}", validRequest);
+
+                  UpdateRoomInput updateRoomInput = convertToUpdateRoomInput(validRequest);
+                  UpdateRoomOutput output = hotelClient.updateRoom(validRequest.getRoomId(), updateRoomInput);
+
+                  UpdateRoomResponse response = createResponse(output);
+                  log.info("End update room output: {}", response);
+                  return response;
                 })
                 .toEither()
                 .mapLeft(t -> Match(t).of(
+                    feignCase(),
                     defaultCase(t)
                 ))
         );
+  }
+
+  private UpdateRoomResponse createResponse(UpdateRoomOutput output) {
+    UpdateRoomResponse response = UpdateRoomResponse.builder()
+        .id(output.getId())
+        .build();
+    return response;
+  }
+
+  private UpdateRoomInput convertToUpdateRoomInput(UpdateRoomRequest request) {
+    RoomInput roomInput = conversionService.convert(request.getRoomDetailsRequest(), RoomInput.class);
+    UpdateRoomInput updateRoomInput = UpdateRoomInput.builder()
+        .roomInput(roomInput)
+        .build();
+    return updateRoomInput;
   }
 }
