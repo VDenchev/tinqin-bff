c59d5c7656baf9b0b79fa409812db8e5b9eea08b
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-20 23:38:03 +0300

    [TB-18] Add logic for deleting room

--- core/src/main/java/com/tinqinacademy/bff/core/processors/DeleteRoomOperationProcessor.java
+++ core/src/main/java/com/tinqinacademy/bff/core/processors/DeleteRoomOperationProcessor.java
@@ -4,32 +4,54 @@ import com.tinqinacademy.bff.api.errors.ErrorResponse;
 import com.tinqinacademy.bff.api.operations.deleteroom.operation.DeleteRoomOperation;
 import com.tinqinacademy.bff.api.operations.deleteroom.request.DeleteRoomRequest;
 import com.tinqinacademy.bff.api.operations.deleteroom.response.DeleteRoomResponse;
+import com.tinqinacademy.hotel.api.operations.deleteroom.output.DeleteRoomOutput;
+import com.tinqinacademy.hotel.restexport.client.HotelClient;
 import io.vavr.control.Either;
 import io.vavr.control.Try;
 import jakarta.validation.Validator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
 import static io.vavr.API.Match;
 
+@Slf4j
 @Service
 public class DeleteRoomOperationProcessor extends BaseOperationProcessor implements DeleteRoomOperation {
 
-  public DeleteRoomOperationProcessor(ConversionService conversionService, Validator validator) {
+  private final HotelClient hotelClient;
+
+  public DeleteRoomOperationProcessor(
+      ConversionService conversionService, Validator validator,
+      HotelClient hotelClient
+  ) {
     super(conversionService, validator);
+    this.hotelClient = hotelClient;
   }
 
   @Override
-  public Either<? extends ErrorResponse, DeleteRoomResponse> process(DeleteRoomRequest input) {
-    return validateInput(input)
-        .flatMap(validInput ->
+  public Either<? extends ErrorResponse, DeleteRoomResponse> process(DeleteRoomRequest request) {
+    return validateInput(request)
+        .flatMap(validRequest ->
             Try.of(() -> {
-                  return DeleteRoomResponse.builder().build();
+                  log.info("Start delete room input: {}", validRequest);
+
+                  DeleteRoomOutput output = hotelClient.deleteRoom(validRequest.getId());
+
+                  DeleteRoomResponse response = createResponse(output);
+                  log.info("End delete room output: {}", validRequest);
+                  return response;
                 })
                 .toEither()
                 .mapLeft(t -> Match(t).of(
+                    feignCase(),
                     defaultCase(t)
                 ))
         );
+  }
+
+  private DeleteRoomResponse createResponse(DeleteRoomOutput output) {
+    return DeleteRoomResponse.builder()
+        .build();
   }
 }
