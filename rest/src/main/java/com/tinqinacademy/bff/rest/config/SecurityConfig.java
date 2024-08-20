fed2ad5c1dfe33a33d8c024f3572ea3db2d3adc3
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-20 10:56:34 +0300

    [TB-16] Add dockerfile

--- rest/src/main/java/com/tinqinacademy/bff/rest/config/SecurityConfig.java
+++ rest/src/main/java/com/tinqinacademy/bff/rest/config/SecurityConfig.java
@@ -17,8 +17,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -51,6 +49,7 @@ public class SecurityConfig {
             .requestMatchers(
                 "/v3/api-docs/**",
                 "/swagger-ui/**",
+                "/actuator/**",
                 "/swagger-ui.html").permitAll()
             .anyRequest().authenticated())
         .httpBasic(Customizer.withDefaults())
