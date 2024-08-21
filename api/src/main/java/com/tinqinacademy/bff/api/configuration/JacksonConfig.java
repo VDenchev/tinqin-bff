03be4767358d7b8108c8d7726113f08c9cbbea15
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-21 22:46:14 +0300

    [TB-20] Implement get comments logic. Replace page from spring jpa with custom fields. Remove spring jpa dependency

--- api/src/main/java/com/tinqinacademy/bff/api/configuration/JacksonConfig.java
+++ api/src/main/java/com/tinqinacademy/bff/api/configuration/JacksonConfig.java
@@ -3,8 +3,6 @@ package com.tinqinacademy.bff.api.configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.cloud.openfeign.support.PageJacksonModule;
-import org.springframework.cloud.openfeign.support.SortJacksonModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +11,7 @@ public class JacksonConfig {
 
   @Bean
   public ObjectMapper objectMapper() {
-    return new ObjectMapper().registerModules(new JavaTimeModule(), new PageJacksonModule(), new SortJacksonModule())
+    return new ObjectMapper().registerModules(new JavaTimeModule())
         .enable(SerializationFeature.INDENT_OUTPUT)
         .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
         .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
--- core/src/main/java/com/tinqinacademy/bff/core/processors/GetCommentsOperationProcessor.java
+++ core/src/main/java/com/tinqinacademy/bff/core/processors/GetCommentsOperationProcessor.java
@@ -1,35 +1,71 @@
 package com.tinqinacademy.bff.core.processors;
 
 import com.tinqinacademy.bff.api.errors.ErrorResponse;
+import com.tinqinacademy.bff.api.models.response.CommentDetailsResponse;
 import com.tinqinacademy.bff.api.operations.getcomments.operation.GetCommentsOperation;
 import com.tinqinacademy.bff.api.operations.getcomments.request.GetCommentsRequest;
 import com.tinqinacademy.bff.api.operations.getcomments.response.GetCommentsResponse;
+import com.tinqinacademy.comments.api.operations.getcomments.output.GetCommentsOutput;
+import com.tinqinacademy.comments.restexport.client.CommentsClient;
+import com.tinqinacademy.hotel.restexport.client.HotelClient;
 import io.vavr.control.Either;
 import io.vavr.control.Try;
 import jakarta.validation.Validator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 import static io.vavr.API.Match;
 
+@Slf4j
 @Service
 public class GetCommentsOperationProcessor extends BaseOperationProcessor implements GetCommentsOperation {
 
-  public GetCommentsOperationProcessor(ConversionService conversionService, Validator validator) {
+  private final HotelClient hotelClient;
+  private final CommentsClient commentsClient;
+
+  public GetCommentsOperationProcessor(
+      ConversionService conversionService, Validator validator,
+      HotelClient hotelClient, CommentsClient commentsClient
+  ) {
     super(conversionService, validator);
+    this.hotelClient = hotelClient;
+    this.commentsClient = commentsClient;
   }
 
   @Override
-  public Either<? extends ErrorResponse, GetCommentsResponse> process(GetCommentsRequest input) {
-    return validateInput(input)
-        .flatMap(validInput ->
+  public Either<? extends ErrorResponse, GetCommentsResponse> process(GetCommentsRequest request) {
+    return validateInput(request)
+        .flatMap(validRequest ->
             Try.of(() -> {
-                  return GetCommentsResponse.builder().build();
+                  log.info("Start get comments input: {}", validRequest);
+
+                  hotelClient.getRoom(validRequest.getRoomId());
+
+                  GetCommentsOutput output = commentsClient.getComments(validRequest.getRoomId(),
+                      validRequest.getPageNumber(), validRequest.getPageSize());
+
+                  GetCommentsResponse response = createResponse(output);
+                  log.info("End get comments input: {}", response);
+                  return response;
                 })
                 .toEither()
                 .mapLeft(t -> Match(t).of(
+                    feignCase(),
                     defaultCase(t)
                 ))
         );
+  }
+
+  private GetCommentsResponse createResponse(GetCommentsOutput output) {
+    GetCommentsResponse response = conversionService.convert(output, GetCommentsResponse.class);
+    List<CommentDetailsResponse> commentDetailsResponseList = output.getComments()
+        .stream()
+        .map(c -> conversionService.convert(c, CommentDetailsResponse.class))
+        .toList();
+    response.setComments(commentDetailsResponseList);
+    return response;
   }
 }
