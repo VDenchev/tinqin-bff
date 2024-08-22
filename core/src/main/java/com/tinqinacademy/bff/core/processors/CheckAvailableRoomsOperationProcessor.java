d832ea1aa4b726249c1539542a381a527eec3922
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-22 20:17:20 +0300

    [TB-22] Implement check available rooms logic

--- core/src/main/java/com/tinqinacademy/bff/core/processors/CheckAvailableRoomsOperationProcessor.java
+++ core/src/main/java/com/tinqinacademy/bff/core/processors/CheckAvailableRoomsOperationProcessor.java
@@ -1,35 +1,63 @@
 package com.tinqinacademy.bff.core.processors;
 
+import com.tinqinacademy.bff.api.enums.BedType;
 import com.tinqinacademy.bff.api.errors.ErrorResponse;
 import com.tinqinacademy.bff.api.operations.checkavailablerooms.operation.CheckAvailableRoomsOperation;
 import com.tinqinacademy.bff.api.operations.checkavailablerooms.request.CheckAvailableRoomsRequest;
 import com.tinqinacademy.bff.api.operations.checkavailablerooms.response.CheckAvailableRoomsResponse;
+import com.tinqinacademy.hotel.api.operations.checkavailablerooms.output.AvailableRoomsOutput;
+import com.tinqinacademy.hotel.restexport.client.HotelClient;
 import io.vavr.control.Either;
 import io.vavr.control.Try;
 import jakarta.validation.Validator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
 import static io.vavr.API.Match;
 
+@Slf4j
 @Service
 public class CheckAvailableRoomsOperationProcessor extends BaseOperationProcessor implements CheckAvailableRoomsOperation {
 
-  public CheckAvailableRoomsOperationProcessor(ConversionService conversionService, Validator validator) {
+  private final HotelClient hotelClient;
+
+  public CheckAvailableRoomsOperationProcessor(ConversionService conversionService, Validator validator, HotelClient hotelClient) {
     super(conversionService, validator);
+    this.hotelClient = hotelClient;
   }
 
   @Override
-  public Either<? extends ErrorResponse, CheckAvailableRoomsResponse> process(CheckAvailableRoomsRequest input) {
-    return validateInput(input)
-        .flatMap(validInput ->
+  public Either<? extends ErrorResponse, CheckAvailableRoomsResponse> process(CheckAvailableRoomsRequest request) {
+    return validateInput(request)
+        .flatMap(validRequest ->
             Try.of(() -> {
-                  return CheckAvailableRoomsResponse.builder().build();
+                  log.info("Start check available rooms input: {}", validRequest);
+
+                  AvailableRoomsOutput output = hotelClient.checkAvailableRooms(
+                      validRequest.getStartDate(),
+                      validRequest.getEndDate(),
+                      validRequest.getBedCount(),
+                      validRequest.getBedSizes(),
+                      validRequest.getBathroomType()
+                  );
+
+                  CheckAvailableRoomsResponse response = createResponse(output);
+                  log.info("End check available rooms output: {}", response);
+                  return response;
                 })
                 .toEither()
                 .mapLeft(t -> Match(t).of(
+                    feignCase(),
                     defaultCase(t)
                 ))
         );
+  }
+
+  private static CheckAvailableRoomsResponse createResponse(AvailableRoomsOutput output) {
+    CheckAvailableRoomsResponse response = CheckAvailableRoomsResponse.builder()
+        .roomIds(output.getRoomIds())
+        .build();
+    return response;
   }
 }
